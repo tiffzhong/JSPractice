7087a8e669a980c286820c51238ab724e96ed684
Tiffany Zhong <tiffzhong@gmail.com>
2019-04-23 16:45:39 -0700

    dates and time

---
+++ datesAndTime/dates.js
@@ -0,0 +1,16 @@
+//years months days and time
+console.log("date", new Date());
+console.log("getFullYear", new Date().getFullYear());
+console.log("getMonth", new Date().getMonth());
+console.log("getDate", new Date().getDate());
+console.log("getDay", new Date().getDay()); //0 is Sunday, 1 is Monday,...
+console.log("getHours", new Date().getHours());
+console.log("getMinutes", new Date().getMinutes());
+console.log("getSeconds", new Date().getSeconds());
+
+//timestamp
+console.log("getTime", new Date().getTime()); //number of milliseconds since jan 1
+
+//date strings
+console.log("toDateString", new Date().toDateString());
+console.log("toDateString", new Date().toTimeString());
--- todo/todo.js
+++ todo/todo.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
 const generateTemplate = todo => {
   const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
   <span>${todo}</span><i class="far fa-trash-alt delete"></i></li>`;
@@ -7,6 +8,7 @@ const generateTemplate = todo => {
   list.innerHTML += html; //adding the HTML to the list
 };
 
+//ADDING A TODO
 addForm.addEventListener("submit", e => {
   e.preventDefault();
   const todo = addForm.add.value.trim();
@@ -19,8 +21,32 @@ addForm.addEventListener("submit", e => {
   }
 });
 
+//DELETE A TODO
 list.addEventListener("click", e => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove(); //removes the li tag from the list
   }
+});
+
+//SEARCH A TODO
+
+//HIDING TODOS THAT ARENT IN THE SERACH QUERY
+const filteredTodo = searchTerm => {
+  Array.from(list.children)
+    .filter(
+      todoItem => !todoItem.textContent.toLowerCase().includes(searchTerm)
+    )
+    .forEach(todo => todo.classList.add("filtered"));
+  //.textContent looks at the text content in the todo li item
+  //.add adding a "filtered" classname to li tag that doesn't have search term
+
+  Array.from(list.children)
+    .filter(todoItem => todoItem.textContent.toLowerCase().includes(searchTerm))
+    .forEach(todo => todo.classList.remove("filtered"));
+  //.remove when we have a match, we take the filtered classname off
+};
+//Searched Term
+search.addEventListener("keyup", () => {
+  const searchTerm = search.value.trim().toLowerCase();
+  filteredTodo(searchTerm);
 });
