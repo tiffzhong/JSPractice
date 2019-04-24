431aa6da50d7d93555cd7db51f9ca9d0865ce3b9
Tiffany Zhong <tiffzhong@gmail.com>
2019-04-24 15:40:13 -0700

    weather done

--- weather/scripts/app.js
+++ weather/scripts/app.js
@@ -1,12 +1,15 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
-const time = document.querySelector(".img.time");
-const icon = document.querySelector(".icon.img");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 const updateUI = data => {
   // const cityDetails = data.cityDetails;
   // const weatherCondition = data.weatherCondition;
+
   const { cityDetails, weatherCondition } = data;
+
+  //update weather details
   details.innerHTML = ` 
   <h5 class="my-3">${cityDetails.EnglishName}</h5>
   <div class="my-3">${weatherCondition.WeatherText}</div>
@@ -15,7 +18,15 @@ const updateUI = data => {
     <span>${weatherCondition.Temperature.Imperial.Value}</span>
     <span>&deg;F</span>
   </div>`;
+  //update night and day background icon
+  let iconSrc = `img/icons/${weatherCondition.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
 
+  //update night and day background image
+  let timeSrc = weatherCondition.IsDayTime ? "img/day.svg" : "img/night.svg";
+  time.setAttribute("src", timeSrc);
+
+  //remove display: none if present
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none");
   }
