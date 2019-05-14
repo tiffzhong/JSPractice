969229998dfe57d0167e8ae3ab81570654591449
Tiffany Zhong <tiffzhong@gmail.com>
2019-05-13 17:09:46 -0700

    recipe app

---
+++ firebase/firebase.js
@@ -0,0 +1,67 @@
+const list = document.querySelector("ul");
+const form = document.querySelector("form");
+
+const addRecipe = (recipe, id) => {
+  let time = recipe.created_at.toDate();
+  let html = `
+  <li data-id="${id}">
+  <div>${recipe.title}</div>
+  <div>${time}</div>
+  <button class="btn btn-danger btn-sm my-2">delete</button>
+  </li>`;
+  list.innerHTML += html;
+};
+
+const deleteRecipe = id => {
+  const recipes = document.querySelectorAll("li");
+  recipes.forEach(recipe => {
+    if (recipe.getAttribute("data-id") === id) {
+      recipe.remove();
+    }
+  });
+};
+
+// get data
+db.collection("recipes").onSnapshot(snapshot => {
+  snapshot.docChanges().forEach(change => {
+    const doc = change.doc;
+    console.log(doc);
+    if (change.type === "added") {
+      addRecipe(doc.data(), doc.id);
+    } else if (change.type === "removed") {
+      deleteRecipe(doc.id);
+    }
+  });
+});
+
+// add documents
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const now = new Date();
+  const recipe = {
+    title: form.recipe.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now)
+  };
+
+  db.collection("recipes")
+    .add(recipe)
+    .then(() => {
+      console.log("recipe added");
+    })
+    .catch(error => console.log(error));
+
+  form.reset();
+});
+
+//delete data
+list.addEventListener("click", e => {
+  if (e.target.tagName === "BUTTON") {
+    const id = e.target.parentElement.getAttribute("data-id");
+    console.log(id);
+    db.collection("recipes")
+      .doc(id)
+      .delete()
+      .then(() => console.log("recipe deleted"))
+      .catch(error => console.log(error));
+  }
+});
